be725ac299dab4d8430cd24ca51aeb4c3d44d97d
C0A21021 <c0a2102127@edu.teu.ac.jp>
2022-11-22 18:56:11 +0900

    アルファベット(編集後)

--- ProjExD2022/ex01/alphabet.py
+++ ProjExD2022/ex01/alphabet.py
@@ -1,5 +1,7 @@
 import random
+import datetime
 
+num_of_alphabet = 26
 tyousen_kaisu = 10
 ketuson_mozi = 2
 max_kaisu = 3
@@ -12,19 +14,13 @@ def shutudai(alf_lst):
     [print(i, end = " ") for i in alf]
     print("\n欠損文字")
     [print(i, end = " ") for i in del_alf]
-    return del_alf
-    for i in alf:
-        if i in del_alf:
-            #print(i)
-            alf.remove(i)
-        else:
-            pass
     print("\n表示文字")
-    [print(i, end = " ") for i in alf]
-
+    [print(i, end = " ") for i in alf if i not in del_alf]
+    return del_alf
 
 
 def kaitou(del_alf):
+    st = datetime.datetime.now()
     flag = True
     ans = int(input("\n欠損文字列はいくつあるでしょうか？"))
     if ans == ketuson_mozi:
@@ -40,11 +36,13 @@ def kaitou(del_alf):
     else:
         print("不正解です。またチャレンジしてください")
         flag = False
+    ed = datetime.datetime.now()
+    print("回答時間：" + str((ed - st).seconds) + "秒")
     return flag
     
 
 if __name__ == "__main__":
-    alf_lst = ["a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m","n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    alf_lst = [chr(i + 65)for i in range(num_of_alphabet)]
     for i in range(max_kaisu):
         ans_lst = shutudai(alf_lst)
         flag = kaitou(ans_lst)
