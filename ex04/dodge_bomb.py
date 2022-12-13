d601327500e899024f78390bf7407fcaf6ac25e2
C0A21021 <c0a2102127@edu.teu.ac.jp>
2022-12-13 18:10:52 +0900

    #10

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -16,7 +16,7 @@ def check_bound(obj_rct, scr_rct):
 
 def main():
     #練習1
-    pg.display.set_caption("逃げろ！こうかとん") #タイトルバーに「初めてのPyGame」と表示する
+    pg.display.set_caption("逃げろ！こうかとん") #タイトルバーに「逃げろ!こうかとん」と表示する
     scrn_sfc = pg.display.set_mode((1600,900))
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
     scrn_rct = scrn_sfc.get_rect()
