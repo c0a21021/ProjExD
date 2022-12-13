05f89a0b428a5d9c3c92dbf9fad84731a2e8bf20
C0A21021 <c0a2102127@edu.teu.ac.jp>
2022-12-13 14:17:02 +0900

    練習2

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -6,10 +6,15 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん") #タイトルバーに「初めてのPyGame」と表示する
     scrn_sfc = pg.display.set_mode((1600,900))
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
+    pgbg_rect = pgbg_sfc.get_rect()
 
     #練習2
-    #while True:
-     #   for event in pg.event.get
+    while True:
+        scrn_sfc.blit(pgbg_sfc, pgbg_rect)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+        pg.display.update()
 
 if __name__ == '__main__':
     main()
